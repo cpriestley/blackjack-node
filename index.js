da906c830db3ab6517dd05da36781bea454224db
Clayton Priestley <clayton.priestley@gmail.com>
2022-12-10 23:07:26 -0600

    replace index.js with game.js. remove 'chalk' and add 'readline' to move cursor position

--- index.js
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env node
-const { program } = require('commander');
-const { game } = require('./blackjack/game');
-
-program
-    .name("blackjack")
-    .description("simulates a casino banking game of Blackjack")
-    .action(game)
